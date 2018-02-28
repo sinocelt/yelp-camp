c7a438e4fe2f8146134185cc134cc3fdd313db60
Justin Barry <sinocelt@users.noreply.github.com>
2018-02-28 18:43:28 +0800

    Update comment.js

--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var commentSchema = mongoose.Schema({
     text: String,
-    author: "String"
+    author: String
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
